b9dbeea20b3f3813970321828bea0f5d3b0483c7
afsori <afsoryafsor@gmail.com>
2020-01-04 21:31:04 +0700

    cors

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const server = express();
 const port = process.env.PORT;
 const nodeEnv = "Development";
 // ||'8080'
-server.use(cors());
 
 server.listen(port, () => {
   console.log(`Server is running in port ${port} in ${nodeEnv} Mode`);
 });
+server.use(cors());
 // server.use(logger("dev"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
