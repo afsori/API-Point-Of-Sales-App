3a1ca6af08c77072e80a576bf0be73faae613917
afsori <afsoryafsor@gmail.com>
2020-01-04 19:29:54 +0700

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 require("dotenv/config");
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const bodyParser = require("body-parser");
 const Router = require("./src/Routes/index");
 const server = express();
@@ -14,7 +14,7 @@ server.use(cors());
 server.listen(port, () => {
   console.log(`Server is running in port ${port} in ${nodeEnv} Mode`);
 });
-server.use(logger("dev"));
+// server.use(logger("dev"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use("/", Router);
